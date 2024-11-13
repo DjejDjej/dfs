7842bf7ef25affff428bf20bd042db9ed50df7fb
Your Name <you@example.com>
2024-11-13 02:25:17 +0100

    help

--- config.h
+++ config.h
@@ -1,15 +1,15 @@
-
 // Define the Month struct
 typedef struct {
-    const char cz[10];
-    const char en[10];
+  const char cz[10];
+  const char en[10];
+  const char czD[10];
 } Month;
 
 // Define the array of months
 Month m[] = {
-    {"leden", "january"}, {"unor", "february"},     {"brezen", "march"},
-    {"duben", "april"},   {"kveten", "may"},        {"cerven", "june"},
-    {"cervenec", "july"}, {"srpen", "august"},      {"zari", "september"},
-    {"rijen", "october"}, {"listopad", "november"}, {"prosinec", "december"}
-};
-
+    {"leden", "january", "leden"},        {"unor", "february", "únor"},
+    {"brezen", "march", "březen"},        {"duben", "april", "duben"},
+    {"kveten", "may", "květen"},          {"cerven", "june", "červen"},
+    {"cervenec", "july", "červenec"},     {"srpen", "august", "srpen"},
+    {"zari", "september", "září"},        {"rijen", "october", "říjen"},
+    {"listopad", "november", "listopad"}, {"prosinec", "december", "prosinec"}};
--- dfs.c
+++ dfs.c
@@ -1,22 +1,32 @@
+#include "config.h"
 #include <ctype.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include "config.h"
+#include <string.h>
 
+void printHelp() {
+  printf("Usage: ./program <month>\n");
+  printf(" - Enter month as a number (1-12) or name (e.g., \"January\" or "
+         "\"Prosinec\").\n");
+  printf("Examples:\n");
+  printf("   ./program 1\n");
+  printf("   ./program January\n");
+  printf("   ./program Pro\n");
+}
 
 int printMonth(int mi, int s) {
   if (s == 1)
     mi--;
 
-  printf("CZ: %s\nEN: %s\nNumeric: %d\n", m[mi].cz, m[mi].en, mi + 1);
+  printf("CZ: %s\nEN: %s\nNumeric: %d\n", m[mi].czD, m[mi].en, mi + 1);
   fflush(stdout);
   return 0;
 }
 
 int fromName(char *arg) {
   for (size_t j = 0; j <= 11; j++) {
-    if ((strcmp(m[j].cz, arg) == 0) || (strcmp(m[j].cz, arg)) == 0) {
+    if ((strcmp(m[j].cz, arg) == 0) || (strcmp(m[j].en, arg)) == 0 ||
+        (strcmp(m[j].czD, arg) == 0)) {
       printMonth(j, 69);
       return 0;
     }
@@ -55,10 +65,14 @@ int findMonth(char *arg) {
 int main(int argc, char *argv[]) {
 
   if (argc != 2) {
-    return 0;
+    printHelp();
+    return 1;
+    printf("Invalid month input.\n");
   }
 
   if (findMonth(argv[1]) != 0) {
+
+    printHelp();
     printf("Invalid month input.\n");
     return 1;
   }
